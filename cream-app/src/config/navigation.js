d446439f38d44e4f1678ba62b6d66aa544cb39ca
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-05-04 16:06:20 +0500

    Update

--- cream-app/src/config/navigation.js
+++ cream-app/src/config/navigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import  Ionicons from 'react-native-vector-icons/Ionicons'
 
 import Dashboard from '../screen/Dashboard';
 import Pickup from '../screen/Pickup';
@@ -10,6 +11,10 @@ import RideHistory from '../screen/rideHistory';
 import RideHistoryDetail from '../screen/rideHistoryDetail';
 import Status from '../screen/Status';
 import Splash from '../screen/Splash';
+import Profile from '../screen/Profile';
+import Message from '../screen/Message';
+import Setting from '../screen/Setting';
+import Custondrawer from '../components/Custondrawer';
 
 
 const Stack = createNativeStackNavigator();
@@ -39,9 +44,22 @@ function MainNavigator() {
 
 function MyDrawer() {
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Dashboard" component={Dashboard} />
-            <Drawer.Screen name="Ride History" component={RideHistory} />
+        <Drawer.Navigator drawerContent={props=> <Custondrawer {...props}/>} 
+        screenOptions={{drawerActiveBackgroundColor:'green',drawerActiveTintColor:'white',drawerInactiveTintColor:'#333',
+        drawerLabelStyle:{marginLeft:-22,fontSize:15}}}
+        >
+            <Drawer.Screen name="Dashboard" component={Dashboard} options={{drawerIcon:({color})=>(
+                <Ionicons name='home-outline' size={22} color={color}/>
+            )}}/>
+            <Drawer.Screen name="Profile" component={Profile} options={{drawerIcon:({color})=>(
+                <Ionicons name='person-outline' size={22} color={color}/>
+            )}} />
+            <Drawer.Screen name="Message" component={Message} options={{drawerIcon:({color})=>(
+                <Ionicons name='chatbox-ellipses-outline' size={22} color={color}/>
+            )}} />
+            <Drawer.Screen name="Setting" component={Setting} options={{drawerIcon:({color})=>(
+                <Ionicons name='settings-outline' size={22} color={color}/>
+            )}} />
         </Drawer.Navigator>
     );
 }
